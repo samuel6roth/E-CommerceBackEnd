2b5d85097a5db162ab3db35e55c416fda930531b
Samuel Roth <samuel6roth@gmail.com>
2021-05-03 18:30:24 -0500

    Models work

--- models/index.js
+++ models/index.js
@@ -3,14 +3,36 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const { triggerAsyncId } = require('node:async_hooks');
+
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
+
 
 // Categories have many Products
+Category.hasMany(Product, {
+  onDelete: 'Cascade',
+  foreignKey: 'category_id'
+});
+
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: 'product_tag',
+  foreignKey: 'product_id'
+});
+
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: 'product_tag',
+  foreignKey: 'tag_id'
+});
+
+
 
 module.exports = {
   Product,
