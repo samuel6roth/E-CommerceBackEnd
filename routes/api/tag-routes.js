7787220e7154277bb0a3f854379a17a89bc6c8e4
Samuel Roth <samuel6roth@gmail.com>
2021-05-03 22:38:53 -0500

    Had wrong model called

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -18,7 +18,7 @@ router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   try {
-    const tagData = await Product.findByPk(req.params.id, { include: [{ model: Product }] });
+    const tagData = await Tag.findByPk(req.params.id, { include: [{ model: Product }] });
     if (!tagData) {
       res.status(404).json({message: 'Tag with that ID not found.'});
       return;
