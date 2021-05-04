da92b0e519c51dc3d3d0d36927dcc6e10aa15083
Samuel Roth <samuel6roth@gmail.com>
2021-05-03 23:42:15 -0500

    Final syntax error corrections

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -33,7 +33,7 @@ router.get('/:id', async (req, res) => {
 });
 
 // create new product
-router.post('/', async (req, res) => {
+router.post('/', (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -65,7 +65,7 @@ router.post('/', async (req, res) => {
 });
 
 // update product
-router.put('/:id', async (req, res) => {
+router.put('/:id', (req, res) => {
   // update product data
   Product.update(req.body, {
     where: {
